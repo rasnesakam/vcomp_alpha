412d5951a9d8b22ee960a70db7ef351ae4ff7571
ensar <ensar.makas@gmail.com>
2022-12-11 09:22:01 +0300

    added camera support. We will start to implement cnn algorithm

--- vcomp.py
+++ vcomp.py
@@ -4,8 +4,14 @@ import cv2
 import numpy as np
 
 def vcomp():
-	resim = cv2.imread("its_tanley_bitch.png")
-	print(resim)
-	cv2.imshow("program",resim)
-	cv2.waitKey(0)
+	#image = cv2.imread("its_tanley_bitch.png",0)
+	#print(image)
+	#cv2.imshow("program",image)
+	camera = cv2.VideoCapture(0)
+	while True:
+		ret, frame = camera.read()
+		cv2.imshow("video",frame)
+		if cv2.waitKey(10) & 0xFF == ord("q"):
+			break
+	camera.release()
 	cv2.destroyAllWindows()
